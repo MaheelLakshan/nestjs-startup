88cefec5339ebef7ee4872b798408ae58880e2fb
MaheelLakshan <MaheelLakshanra@gmail.com>
2024-11-25 12:14:55 +0530

    creating modules

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,8 @@
+import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller()
+export class AuthController {
+  //dependacy injection for instantiating the service in the controller
+  constructor(private authService: AuthService) {}
+}
