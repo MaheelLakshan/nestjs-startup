47fbad20e4aac88ba5380460f4ded16bc4e0d726
MaheelLakshan <MaheelLakshanra@gmail.com>
2024-11-25 12:16:28 +0530

    calling method in service from controller dependancy injection

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,5 +4,7 @@ import { AuthService } from './auth.service';
 @Controller()
 export class AuthController {
   //dependacy injection for instantiating the service in the controller
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {
+    authService.test();
+  }
 }
